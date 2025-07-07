aef3c7eea86f9e7cf8a2cfc2043276c6539b4480
zaka <103366844+zakaBouj@users.noreply.github.com>
2025-07-07 20:55:12 +0200

    Update 06_Pulse-Width-Modulation.ino

--- Arduino/06_Pulse-Width-Modulation/06_Pulse-Width-Modulation/06_Pulse-Width-Modulation.ino
+++ Arduino/06_Pulse-Width-Modulation/06_Pulse-Width-Modulation/06_Pulse-Width-Modulation.ino
@@ -1,34 +1,30 @@
 // RTS SoSe 2025
 // Zakaria Boujana
 // Pulse-Width-Modulation
-const int potPin = A1;        // Potentiometer on analog pin A1
-const int pwmPin = 9;         // PWM output pin
-const int digitalReadPin = 2; // Digital input to read PWM signal
+const int potPin = A1;        
+const int pwmPin = 9;         
+const int digitalReadPin = 2; 
 
 void setup() {
-  Serial.begin(2000000);      // High baud rate for fast sampling
-  pinMode(pwmPin, OUTPUT);    // Set PWM pin as output
-  pinMode(digitalReadPin, INPUT); // Set digital pin as input
+  Serial.begin(2000000);      
+  pinMode(pwmPin, OUTPUT);
+  pinMode(digitalReadPin, INPUT);
 }
 
 void loop() {
-  // Read potentiometer (0-1023)
   int potValue = analogRead(potPin);
   
-  // Map to PWM range (0-255)
   int pwmValue = map(potValue, 0, 1023, 0, 255);
   
-  // Write PWM value
   analogWrite(pwmPin, pwmValue);
   
-  // Read the PWM signal on digital input
   int digitalValue = digitalRead(digitalReadPin);
   
   // Output to Serial Plotter
   Serial.println(digitalValue, DEC);
   
   // Very short or no delay for high-frequency sampling
-  delayMicroseconds(50); // 50 microseconds delay
+  delayMicroseconds(50);
 }
 
 // HOW TO VIEW THE PWM SIGNAL:
